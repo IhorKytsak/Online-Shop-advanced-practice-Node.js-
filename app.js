78acc2426cbb6fde196bcdb1dfc4720cf4f67ace
Ihor Kytsak <kitsak735@gmail.com>
2022-08-08 19:42:17 +0300

    improve pug layout

--- app.js
+++ app.js
@@ -2,23 +2,30 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const { engine } = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+// Using pug engine
+// app.set('view engine', 'pug');
+// app.set('views', 'views');
+
+//Using handlebars engine
+app.engine('hbs', engine());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404');
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);

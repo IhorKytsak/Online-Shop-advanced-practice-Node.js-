0a282fb8fd9ee237de0c3f436b46b8f05dda62c3
Ihor Kytsak <kitsak735@gmail.com>
2022-08-10 18:46:17 +0300

    added edit , delete posibilities + refactoring

--- controllers/shop.js
+++ controllers/shop.js
@@ -7,22 +7,19 @@ exports.getProducts = (req, res, next) => {
       prods: products,
       pageTitle: 'All Products',
       path: '/products',
-      // hasProducts: products.length > 0, //for handlebars
-      // activeShop: true,
-      // productCSS: true,
     });
   });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) =>
+  Product.findById(prodId, (product) => {
     res.render('shop/product-detail', {
       product: product,
       pageTitle: product.title,
       path: '/products',
-    })
-  );
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
@@ -36,9 +33,23 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    pageTitle: 'Your Cart',
-    path: '/cart',
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: cartProducts,
+      });
+    });
   });
 };
 
@@ -50,16 +61,24 @@ exports.postCart = (req, res, next) => {
   res.redirect('/cart');
 };
 
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
+};
+
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
-    pageTitle: 'Your Orders',
     path: '/orders',
+    pageTitle: 'Your Orders',
   });
 };
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
-    pageTitle: 'Checkout',
     path: '/checkout',
+    pageTitle: 'Checkout',
   });
 };
